c4ef17f27b5ef6ba84100e6759ea893f7d2573b5
CiaraBrown1596 <ciarabrown1596@gmail.com>
2021-08-16 16:05:20 +0100

    search bar and buttons in layout file

--- app/src/main/java/com/example/pubportal/GoogleMapsActivity.java
+++ app/src/main/java/com/example/pubportal/GoogleMapsActivity.java
@@ -3,9 +3,15 @@ package com.example.pubportal;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +23,14 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
 
 public class GoogleMapsActivity extends AppCompatActivity {
 
@@ -35,8 +45,6 @@ public class GoogleMapsActivity extends AppCompatActivity {
 
         smf = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
         client = LocationServices.getFusedLocationProviderClient(this);
-
-
 
 
         if (ActivityCompat.checkSelfPermission(GoogleMapsActivity.this,
@@ -63,35 +71,76 @@ public class GoogleMapsActivity extends AppCompatActivity {
         }
     }
 
-    private void getCurrentLocation() {
-            @SuppressLint("MissingPermission") Task<Location> task = client.getLastLocation();
-            task.addOnSuccessListener(new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    //when success
-                    if (location != null) {
-                        //sync map
-                        smf.getMapAsync(new OnMapReadyCallback() {
-                            @Override
-                            public void onMapReady(GoogleMap googleMap) {
-                                //initalize laglng
+    public void onCLick(View v) {
+        switch (v.getId()) {
+            case R.id.searchBtn:
+                EditText searchedAddress = (EditText) findViewById(R.id.locationSearch);
+                String address = searchedAddress.getText().toString();
 
-                                LatLng latLng = new LatLng(location.getLatitude(),
-                                        location.getLongitude());
-                                // create maker options
-                                MarkerOptions options = new MarkerOptions().position(latLng)
-                                        .title("You are here...");
-                                //zoom map
-                                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-                                //add marker to map
-                                googleMap.addMarker(options);
+                List<Address> addressList = null;
+                MarkerOptions userMarkerOptions = new MarkerOptions();
+
+                if (!TextUtils.isEmpty(address)) {
+                    Geocoder geocoder = new Geocoder(this);
+
+                    try {
+                        addressList = geocoder.getFromLocationName(address, 6);
+
+                        if (addressList != null) {
+                            for (int i = 0; i < addressList.size(); i++) {
+                                Address userAddress = addressList.get(i);
+                                LatLng latLng = new LatLng(userAddress.getLatitude(), userAddress.getLongitude());
+
+                                userMarkerOptions.position(latLng);
+                                userMarkerOptions.title(address);
+                                userMarkerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN));
+
+
                             }
-                        });
+                        }
+                        else{
+                            Toast.makeText(this,"Location not found...",Toast.LENGTH_LONG).show();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                }
-            });
-        }
 
+                }
+                else{
+                    Toast.makeText(this,"Enter a location name...",Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+
+    private void getCurrentLocation() {
+        @SuppressLint("MissingPermission") Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                //when success
+                if (location != null) {
+                    //sync map
+                    smf.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            //initalize laglng
+
+                            LatLng latLng = new LatLng(location.getLatitude(),
+                                    location.getLongitude());
+                            // create maker options
+                            MarkerOptions options = new MarkerOptions().position(latLng)
+                                    .title("You are here...");
+                            //zoom map
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                            //add marker to map
+                            googleMap.addMarker(options);
+                        }
+                    });
+                }
+            }
+        });
+    }
 
 
 }
